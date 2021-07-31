8868894f58e727d4433ee3bc5bf57e55aa5829ba
Mathieu <mr-zwets@protonmail.com>
2021-07-31 23:03:52 +0200

    show alert on invalid BCH Address & option to remove Addresses from watchlist

--- script.js
+++ script.js
@@ -3,14 +3,7 @@
 const inputField = document.getElementById("search");
 const button = document.getElementById("myBtn");
 let addresses=[]
-if(document.cookie){
-  for(let i=0;i<4;i++){
-    let cookie=document.cookie.match('(^|;)\\s*' + `address${i}` + '\\s*=\\s*([^;]+)')?.pop() || ''
-    if(cookie!=""){addresses=[cookie,...addresses]}
-  }
-  createListWithTemplate(addresses);
-}
-
+getCookies()
 
 // Execute a function when the user releases a key on the keyboard
 inputField.addEventListener("keyup", function(event) {
@@ -21,14 +14,17 @@ inputField.addEventListener("keyup", function(event) {
     }
   });
 
-window.onAdd = function(){
+window.onAdd = async function(){
   let input=inputField.value
   inputField.value=''
-  if(addresses.length>=4){return;}
-  if(addresses.find(address => input==address)!=undefined || input==''){return;}
-  addresses=[input,...addresses]
-  document.cookie = `address${addresses.length-1}=${addresses[0]}`
-  createListWithTemplate(addresses);
+  if(addresses.find(address => input==address)!=undefined || addresses.length>=4){return;}
+  try{
+    await Wallet.watchOnly(input);
+    addresses=[...addresses,input]
+    writeCookies()
+    createListWithTemplate(addresses);
+  } 
+  catch(e){alert("Invalid BCH Address!")}
 }
 
 let stats=[[0.001,"36.9%"],[0.01,"21.43%"],[0.1,"8.35%"],[1,"2.69%"],[10,"0.69%"],[100,"0.08%"],[1000,"0.01%"],[10000,"0.00%"] ]
@@ -40,26 +36,52 @@ function createListWithTemplate(addresses) {
   ul.style="display: flex;min-height: 415px;flex-wrap: wrap;"
   const template = document.getElementById('address-template');
 
-  addresses.forEach(async (address) => {
+  addresses.forEach(async (address, index) => {
     const addressCard = document.importNode(template.content, true);
+    addressCard.querySelector('#x').setAttribute("onclick",`onRemove(${index})`);
     addressCard.querySelector('#addr').textContent=address
-    const wallet = await Wallet.watchOnly(address);
-    let {bch,usd}=await wallet.getBalance();
-    let textBalance=`balance: ${bch} BCH (${usd}$)`
-    addressCard.querySelector('#deposit').src = wallet.getDepositQr().src;
-    addressCard.querySelector('#balance').textContent= textBalance
+    try{
+      const wallet = await Wallet.watchOnly(address);
+      let {bch,usd}=await wallet.getBalance();
+      let textBalance=`balance: ${bch} BCH (${usd}$)`
+      addressCard.querySelector('#deposit').src = wallet.getDepositQr().src;
+      addressCard.querySelector('#balance').textContent= textBalance
 
-    let tiercount=-1
-    stats.forEach(tier => {
-      if(bch>tier[0]){
-        tiercount++
-      } else {return}
-    })
-    if(tiercount>=0){
-      addressCard.querySelector('#richer').textContent=`only ${stats[tiercount][1]} of addresses is richer than ${stats[tiercount][0]}BCH`
-    }
+      let tiercount=-1
+      stats.forEach(tier => {
+        if(bch>tier[0]){
+          tiercount++
+        } else {return}
+      })
+      if(tiercount>=0){
+        addressCard.querySelector('#richer').textContent=`only ${stats[tiercount][1]} of addresses is richer than ${stats[tiercount][0]}BCH`
+      }
 
+    } catch(e){console.log(e)}
     ul.appendChild(addressCard);
   });
   Placeholder.replaceWith(ul);
 };
+
+window.onRemove= function(index){
+  addresses.splice(index, 1)
+  writeCookies();
+  createListWithTemplate(addresses);
+}
+
+function writeCookies(){
+  for(let i=0;i<4;i++){
+    document.cookie =addresses[i]==undefined? `address${i}=`:`address${i}=${addresses[i]}`
+  } 
+}
+
+function getCookies(){
+  if(document.cookie){
+    for(let i=0;i<4;i++){
+      let cookie=document.cookie.match('(^|;)\\s*' + `address${i}` + '\\s*=\\s*([^;]+)')?.pop() || ''
+      if(cookie!=""){addresses=[...addresses,cookie]}
+    }
+  writeCookies()
+  createListWithTemplate(addresses);
+  }
+}
